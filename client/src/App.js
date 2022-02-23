d19d9aa2d76fb7a95a9ff3fa6e64fae8eaaf61c9
leticianardi <leticia.tnardi@gmail.com>
2022-02-22 20:40:25 -0800

    game

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client'
-import {setContext} from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+import Game from "./components/Game/Game.js";
 
 
-const httplink =  createHttpLink({
-  uri: '/graphql'
-})
+const httplink = createHttpLink({
+  uri: "/graphql",
+});
 
-const authlink= setContext((_,{headers})=>{
-  const token =  localStorage.getItem('id_token');
+const authlink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
 
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}`: '',
-    }
-  }
-})
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
   link: authlink.concat(httplink),
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          hellow world
+        </header>
+        <div>
+        <Game />
+        </div>
+      </div>
     </ApolloProvider>
   );
 }
--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -1,85 +1,111 @@
-const tileDisplay = document.querySelector('.tile-container')
-const keyboard = document.querySelector('.key-container')
+import React from "react";
+import "./game.css";
 
-const wordlingo ='SUPER'
-const keys  = [
-    'Q',
-    'W',
-    'E',
-    'R',
-    'T',
-    'Y',
-    'U',
-    'I',
-    'O',
-    'P',
-    'A',
-    'S',
-    'D',
-    'F',
-    'G',
-    'H',
-    'J',
-    'K',
-    'L',
-    'ENTER',
-    'Z',
-    'X',
-    'C',
-    'V',
-    'B',
-    'N',
-    'M',
-    '<<',
-]
-const guessRows = [
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', '']
-]
-let currentRow = 0
-let currentTile = 0
+function Game() {
+  const tileDisplay = document.querySelector(".tile-container");
+  const keyboard = document.querySelector(".key-container");
 
-guessRows.forEach((guessRow,guessRowIndex) => {
-    const rowElement = document.createElement('div')
-    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+  // const wordlingo = "SUPER";
+
+  const keys = [
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "ENTER",
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+    "<<",
+  ];
+
+  const guessRows = [
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ];
+
+  let currentRow = 0;
+  let currentTile = 0;
+
+  guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement("div");
+    rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
     guessRow.forEach((guess, guessIndex) => {
-        const tileElement = document.createElement('div')
-        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
-        tileElement.classList.add('tile')
-        rowElement.append(tileElement)
-    })
-    tileDisplay.append(rowElement)
-})
+      const tileElement = document.createElement("div");
+      tileElement.setAttribute(
+        "id",
+        "guessRow-" + guessRowIndex + "-tile-" + guessIndex
+      );
+      tileElement.classList.add("tile");
+      rowElement.append(tileElement);
+    });
+    tileDisplay.append(rowElement);
+  });
 
-keys.forEach(key => {
-    const buttonElement = document.createElement('button')
-    buttonElement.textContent = key
-    buttonElement.setAttribute('id', key)
-    buttonElement.addEventListener('click', () => handleClick(key))
-    keyboard.append(buttonElement)
-})
+  keys.forEach((key) => {
+    const buttonElement = document.createElement("button");
+    buttonElement.textContent = key;
+    buttonElement.setAttribute("id", key);
+    buttonElement.addEventListener("click", () => handleClick(key));
+    keyboard.append(buttonElement);
+  });
 
-const handleClick = (letter) => {
-    console.log('clicked', letter)
-    if (letter === '<<') {
-        console.log('delete letter')
-        return
+  const handleClick = (letter) => {
+    console.log("clicked", letter);
+    if (letter === "<<") {
+      console.log("delete letter");
+      return;
     }
-    if(letter === 'ENTER') {
-        console.log('')
+    if (letter === "ENTER") {
+      console.log("");
     }
-    addLetter(letter)
+    addLetter(letter);
+  };
+
+  const addLetter = (letter) => {
+    const tile = document.getElementById(
+      "guessRow" + currentRow + "-tile-" + currentTile
+    );
+    tile.textContent = letter;
+    guessRows[currentRow][currentTile] = letter;
+    tile.setAttribute("data", letter);
+    currentTile++;
+    console.log("guessRows", guessRows);
+  };
+
+  return (
+    <div class="game-container">
+      <div class="title-container">
+        <h1>wordlingo</h1>
+      </div>
+      <div class="message-container"></div>
+      <div class="tile-container"></div>
+      <div class="key-container"></div>
+    </div>
+  );
 }
 
-const addLetter = (letter) => {
-    const tile = document.getElementById('guessRow' + currentRow + '-tile-' + currentTile)
-    tile.textContent = letter
-    guessRow[currentRow][currentTile] = letter
-    tile.setAttribute('data', letter)
-    currentTile++
-    console.log('guessRows', guessRows)
-}
+export default Game;
