15f31c605e3768d3d95a1b8258b36e127edae954
leticianardi <leticia.tnardi@gmail.com>
2022-02-22 19:12:10 -0800

    connect back and front end

--- server/models/User.js
+++ server/models/User.js
@@ -20,19 +20,7 @@ const userSchema = new Schema(
       required: true,
       minlength: 5,
     },
-    comments: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Thought",
-      },
-    ],
-    friends: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-  },
+     },
   {
     toJSON: {
       virtuals: true,
@@ -55,9 +43,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-userSchema.virtual("friendCount").get(function () {
-  return this.friends.length;
-});
 
 const User = model("User", userSchema);
 
